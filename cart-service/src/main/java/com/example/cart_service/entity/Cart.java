9f68a2d2a9ab788a63e94c590b6ade38b474d2d0
stanleyshi42 <stanleyshi42@gmail.com>
2024-06-21 10:20:48 -0700

    added base code

---
+++ cart-service/src/main/java/com/example/cart_service/entity/Cart.java
@@ -0,0 +1,28 @@
+package com.example.cart_service.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true)
+    private int userId;
+
+    private ArrayList<Object> products;
+
+    public Cart(int userId) {
+        this(0, userId, new ArrayList<Object>());
+    }
+
+}
