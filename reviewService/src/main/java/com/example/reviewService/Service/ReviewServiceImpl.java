88debb966a70b25eee3bf9bf7c385ef82b18f624
aaron <redskin.rox@gmail.com>
2024-06-21 15:36:03 -0400

    finished controller

--- reviewService/src/main/java/com/example/reviewService/Service/ReviewServiceImpl.java
+++ reviewService/src/main/java/com/example/reviewService/Service/ReviewServiceImpl.java
@@ -2,28 +2,33 @@ package com.example.reviewService.Service;
 
 import com.example.reviewService.Entity.Review;
 import com.example.reviewService.Repository.ReviewDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ReviewServiceImpl {
+    @Autowired
     private ReviewDAO reviewDAO;
-    public ReviewServiceImpl(ReviewDAO reviewDAO){
-        this.reviewDAO = reviewDAO;
-    }
+//    public ReviewServiceImpl(ReviewDAO reviewDAO){
+//        this.reviewDAO = reviewDAO;
+//    }
 
-    Review createReview(Review review){
+    public Review createReview(Review review){
         return this.reviewDAO.save(review);
     }
-    List<Review> getReviews(){
+    public Review updateReview(Review review){
+        return this.reviewDAO.save(review);
+    }
+    public List<Review> getReviews(){
         return this.reviewDAO.findAll();
     }
-    Review getOne(Long id){
-        return this.reviewDAO.findById(id).orElseThrow(()->new RuntimeException("No reviews found"));
+    public Review getOneReview(Long reviewId){
+        return this.reviewDAO.findById(reviewId).orElseThrow(()->new RuntimeException("No reviews found"));
     }
 
-    List<Review> getProductReview(Long productId){
+    public List<Review> getProductReview(Long productId){
         return this.reviewDAO.findByProductId(productId);
     }
 }
