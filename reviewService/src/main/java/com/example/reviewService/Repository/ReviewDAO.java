d64ce57a5c02a88f15cc93e08b2290cec1136335
aaron <redskin.rox@gmail.com>
2024-06-21 13:39:33 -0400

    created the service

--- reviewService/src/main/java/com/example/reviewService/Repository/ReviewDAO.java
+++ reviewService/src/main/java/com/example/reviewService/Repository/ReviewDAO.java
@@ -3,5 +3,8 @@ package com.example.reviewService.Repository;
 import com.example.reviewService.Entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewDAO extends JpaRepository<Review, Long> {
+    List<Review> findByProductId(Long productId);
 }
--- reviewService/src/main/java/com/example/reviewService/Service/ReviewServiceImpl.java
+++ reviewService/src/main/java/com/example/reviewService/Service/ReviewServiceImpl.java
@@ -1,4 +1,29 @@
 package com.example.reviewService.Service;
 
+import com.example.reviewService.Entity.Review;
+import com.example.reviewService.Repository.ReviewDAO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ReviewServiceImpl {
+    private ReviewDAO reviewDAO;
+    public ReviewServiceImpl(ReviewDAO reviewDAO){
+        this.reviewDAO = reviewDAO;
+    }
+
+    Review createReview(Review review){
+        return this.reviewDAO.save(review);
+    }
+    List<Review> getReviews(){
+        return this.reviewDAO.findAll();
+    }
+    Review getOne(Long id){
+        return this.reviewDAO.findById(id).orElseThrow(()->new RuntimeException("No reviews found"));
+    }
+
+    List<Review> getProductReview(Long productId){
+        return this.reviewDAO.findByProductId(productId);
+    }
 }
