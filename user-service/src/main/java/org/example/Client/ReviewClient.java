3c6283aaaa63f0d5290824850e3d4ac05768ddc6
habioye <78454514+habioye@users.noreply.github.com>
2024-06-24 05:25:42 -0500

    Added the review client interface to call the microservice

--- user-service/src/main/java/org/example/Client/ReviewClient.java
+++ user-service/src/main/java/org/example/Client/ReviewClient.java
@@ -1,2 +1,16 @@
-package org.example.Client;public interface ReviewClient {
+package org.example.Client;
+
+package com.genspark.department_service.client;
+
+import com.genspark.department_service.model.Employee;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.service.annotation.HttpExchange;
+
+import java.util.List;
+
+@HttpExchange
+public interface ReviewClient {
+    @PostExchange("/review")
+    public Review addReview(@RequestBody Review review);
 }
