2c79c843a389d7ee17552936390a29686c0eb8d5
stanleyshi42 <stanleyshi42@gmail.com>
2024-06-23 17:40:15 -0700

    added finding the items in a cart

--- cart-service/src/main/java/com/example/cart_service/service/ItemsClient.java
+++ cart-service/src/main/java/com/example/cart_service/service/ItemsClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "item-service")
 public interface ItemsClient {
 
-    @GetMapping("/items/{itemId}")
+    @GetMapping("/items/items/{id}")
     Items findById(@PathVariable long id);
 
 }
