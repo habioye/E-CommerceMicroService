6565c300c1d23235e8bed82b18a7775345f142cd
habioye <78454514+habioye@users.noreply.github.com>
2024-06-24 05:27:01 -0500

    Added the config file for calling the review microservice

--- user-service/src/main/java/org/example/Config/WebClientConfig.java
+++ user-service/src/main/java/org/example/Config/WebClientConfig.java
@@ -1,2 +1,36 @@
-package org.example.Config;public class WebClientConfig {
+package org.example.Config;
+
+import org.example.Client.ReviewClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.reactive.LoadBalancedExchangeFilterFunction;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.service.invoker.HttpExchangeAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+@Configuration
+public class WebClientConfig {
+    @Autowired
+    private LoadBalancedExchangeFilterFunction filterFunction;
+
+
+    @Bean
+    public WebClient reviewWebClient() {
+        return WebClient.builder()
+                .baseUrl("http://review-service")
+                .filter(filterFunction)
+                .build();
+    }
+
+
+    @Bean
+    public ReviewClient employeeClient() {
+        WebClientAdapter webClientAdapter = WebClientAdapter.create(reviewWebClient());
+        HttpServiceProxyFactory httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(webClientAdapter).build();
+        return httpServiceProxyFactory.createClient(ReviewClient.class);
+    }
+
 }
