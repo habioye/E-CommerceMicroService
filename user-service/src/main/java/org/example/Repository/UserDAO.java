6594cc039101b5d1059aa6126f451ff99cd13ab2
habioye <78454514+habioye@users.noreply.github.com>
2024-06-24 05:28:27 -0500

    Created user dao

--- user-service/src/main/java/org/example/Repository/UserDAO.java
+++ user-service/src/main/java/org/example/Repository/UserDAO.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface UserDAO extends JpaRepository<User, Long> {
-    List<User> findByUserId(Long productId);
+//    List<User> findByUserId(Long productId);
 }
